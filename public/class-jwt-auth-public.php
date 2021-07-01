98dcc75938aaf5992557c5c6c311d793dd7b82a6
John Parnefjord <john.parnefjord@gmail.com>
2021-07-01 21:49:21 +0200

    Add missing permission_callback in register_rest_route to silent notices.

--- public/class-jwt-auth-public.php
+++ public/class-jwt-auth-public.php
@@ -75,11 +75,13 @@ class Jwt_Auth_Public
         register_rest_route($this->namespace, 'token', array(
             'methods' => 'POST',
             'callback' => array($this, 'generate_token'),
+            'permission_callback' => '__return_true',
         ));
 
         register_rest_route($this->namespace, 'token/validate', array(
             'methods' => 'POST',
             'callback' => array($this, 'validate_token'),
+            'permission_callback' => '__return_true',
         ));
     }
 
