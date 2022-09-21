b4b1cf8ad2db229ff32234ec41ada0c2e62aa70d
Tmeister <noone@tmeister.net>
2022-09-21 18:28:58 -0500

    WP Code Standards

--- jwt-auth.php
+++ jwt-auth.php
@@ -25,15 +25,15 @@
  */
 
 // If this file is called directly, abort.
-if (!defined('WPINC')) {
-    die;
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path(__FILE__) . 'includes/class-jwt-auth.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-jwt-auth.php';
 
 /**
  * Begins execution of the plugin.
@@ -44,9 +44,9 @@ require plugin_dir_path(__FILE__) . 'includes/class-jwt-auth.php';
  *
  * @since    1.0.0
  */
-function run_jwt_auth()
-{
-    $plugin = new Jwt_Auth();
-    $plugin->run();
+function run_jwt_auth() {
+	$plugin = new Jwt_Auth();
+	$plugin->run();
 }
+
 run_jwt_auth();
