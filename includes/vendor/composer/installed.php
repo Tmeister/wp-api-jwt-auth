f19ceb8444078abbc3ddd8a711b7fcb032af0253
Enrique Chavez <noone@tmeister.net>
2023-09-11 08:19:49 -0600

    Final touches for release 1.3.4
    - Version up

--- includes/vendor/composer/installed.php
+++ includes/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'tmeister/wp-api-jwt-auth',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => '54336850307eb4337d80d67066e40b0ed29b1ce5',
+        'reference' => '449f3d07c2827945108cae1202d8e369c3fe7c51',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../../',
         'aliases' => array(),
@@ -43,7 +43,7 @@
         'tmeister/wp-api-jwt-auth' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => '54336850307eb4337d80d67066e40b0ed29b1ce5',
+            'reference' => '449f3d07c2827945108cae1202d8e369c3fe7c51',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../../',
             'aliases' => array(),
